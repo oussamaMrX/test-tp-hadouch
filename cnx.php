db3cd0c263c793dd6067182d519a6718a24f42c5
Atamni123 <atamniloubna@gmail.com>
2025-10-31 11:53:16 +0100

    loubna1

--- cnx.php
+++ cnx.php
@@ -1,1 +1,27 @@
 <?php echo "Hello, World!"; ?>
+<?php
+
+$host = 'localhost';      
+$db   = 'nom_de_la_base'; 
+$user = 'root';           
+$pass = '';                
+
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+
+
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,       
+    PDO::ATTR_EMULATE_PREPARES   => false,                 
+];
+
+try {
+    
+    $pdo = new PDO($dsn, $user, $pass, $options);
+} catch (PDOException $e) {
+    
+    error_log('Erreur de connexion : ' . $e->getMessage());
+    exit('Impossible de se connecter à la base de données.');
+}
+?>
